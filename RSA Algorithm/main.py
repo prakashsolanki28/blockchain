f1c3a1ff3344fa5ed5e9596a7996daedbc9a3a11
Prakash Solanki <74834984+prakashsolanki28@users.noreply.github.com>
2023-12-21 18:01:07 +0000

    Simple blockchain with RSA algorithm

---
+++ RSA Algorithm/main.py
@@ -0,0 +1,50 @@
+import random
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def mod_inverse(a, m):
+    if gcd(a, m) != 1:
+        return None
+    u1, u2, u3 = 1, 0, a
+    v1, v2, v3 = 0, 1, m
+    while v3 != 0:
+        q = u3 // v3
+        v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
+    return u1 % m
+
+def generate_keypair(p, q):
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = random.randrange(1, phi)
+    while gcd(e, phi) != 1:
+        e = random.randrange(1, phi)
+    d = mod_inverse(e, phi)
+    return ((e, n), (d, n))
+
+def encrypt(public_key, plaintext):
+    e, n = public_key
+    ciphertext = [pow(ord(char), e, n) for char in plaintext]
+    return ciphertext
+
+def decrypt(private_key, ciphertext):
+    d, n = private_key
+    plaintext = [chr(pow(char, d, n)) for char in ciphertext]
+    return ''.join(plaintext)
+
+# Example usage
+p = 61
+q = 53
+public_key, private_key = generate_keypair(p, q)
+message = "Hello, world!"
+encrypted_message = encrypt(public_key, message)
+decrypted_message = decrypt(private_key, encrypted_message)
+
+print("Original message:", message)
+print("Encrypted message:", encrypted_message)
+print("Decrypted message:", decrypted_message)
+
+
+# In[ ]:
